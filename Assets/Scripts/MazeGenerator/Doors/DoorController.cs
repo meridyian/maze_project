cbc80c5973acfb7c158cf937290f0baf4313f264
irem <iremaydinn.7@gmail.com>
2023-02-20 10:09:31 +0300

    room key GO added

--- Assets/Scripts/MazeGenerator/Doors/DoorController.cs
+++ Assets/Scripts/MazeGenerator/Doors/DoorController.cs
@@ -4,18 +4,24 @@ using System.Collections.Generic;
 using Cinemachine;
 using UnityEngine;
 
+
+
 public class DoorController : MonoBehaviour
 {
     // to open and close the door when player enters to the room
     // move the doors up, collider should be seperated from object
     // will be assigned to player
+
+    public bool isDoorLocked = true;
     
     private void OnTriggerEnter(Collider other)
     {
-        
-        
+
+        isDoorLocked = false;
         if (other.gameObject.CompareTag("Door"))
         {
+            // if player has the key of room type 
+            
             Debug.Log("Player just entered");
             other.transform.GetChild(0).position = new Vector3(other.transform.GetChild(0).position.x, 1.5f,
                 other.transform.GetChild(0).position.z);
@@ -25,6 +31,8 @@ public class DoorController : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
+        isDoorLocked = true;
+        
         if (other.gameObject.CompareTag("Door"))
         {
             Debug.Log("Close the door");
